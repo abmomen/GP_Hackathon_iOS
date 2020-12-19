74ff09b9247f9a1889b9db5358cc76e804b7e0d6
Muhammad Ariful Islam <arifparvez@sharetrip.net>
2020-12-19 13:32:11 +0600

    added popular movies collection view

--- App/App/Controllers/ViewController.swift
+++ App/App/Controllers/ViewController.swift
@@ -1,12 +1,32 @@
 import UIKit
-import Alamofire
 
 
 class ViewController: UIViewController {
+    
+    //CV stands for collection view
+    private lazy var popularMoviesCV: UICollectionView = {
+        let layout = UICollectionViewFlowLayout.init()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor =  .red
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupView()
     }
+    
+    private func setupView() {
+        view.backgroundColor = .white
+        view.addSubview(popularMoviesCV)
+        
+        
+        popularMoviesCV.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        popularMoviesCV.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        popularMoviesCV.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        popularMoviesCV.heightAnchor.constraint(equalToConstant: 150.0).isActive = true
+    }
+    
 }
 
