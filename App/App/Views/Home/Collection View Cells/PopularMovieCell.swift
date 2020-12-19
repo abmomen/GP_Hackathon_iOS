51d5d963f7c39f10a19acbced1d7fa1c9e9fff0b
Muhammad Ariful Islam <arifparvez@sharetrip.net>
2020-12-19 15:30:44 +0600

    added dependency for kingfisher

--- App/App/Views/Home/Collection View Cells/PopularMovieCell.swift
+++ App/App/Views/Home/Collection View Cells/PopularMovieCell.swift
@@ -6,12 +6,26 @@
 //
 
 import UIKit
+import Kingfisher
 
 class PopularMovieCell: UICollectionViewCell {
-
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        setupView()
+    }
+    
+    private func setupView() {
+        containerView.layer.cornerRadius = 8.0
+    }
+    
+    func configure(imageUrl: String) {
+        
     }
 
 }
